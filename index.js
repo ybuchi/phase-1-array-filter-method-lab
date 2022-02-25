b4e4c066b7056c12ef3e1c574b397ae049414898
ybuchi <yabuch_ch@hotmail.com>
2022-02-25 09:52:41 -0500

    commit tree ca45cf36a4c923ff17d104755fe4f2da7d141277

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+function findMatching(arr, name){
+    //returns all matching names in the arr
+    //if name matches any element in the arry, push the matching names into a new array
+    let newArray = arr.filter(
+        //This cb function should search for all element that match name
+        arr => arr.toLowerCase() === name.toLowerCase()
+    )
+    return newArray;
+};
+
+function fuzzyMatch(arr, name){
+    let newArray = arr.filter(
+        arr => arr.toLowerCase().startsWith(name.toLowerCase()) === true
+    )
+    return newArray;
+};
+ 
+function matchName(arr, name){
+    let newArray = arr.filter(
+        arr => arr.name === name
+    )
+    return newArray;
+};
+
+
